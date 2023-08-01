a3732fad0e4e588ac6ab8e292442a5d94739bb7a
izadoranetz <izadora.ns@gmail.com>
2023-08-01 15:16:47 -0300

    criando component summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,30 @@
+import { ArrowCircleUp, ArrowCircleDown } from "phosphor-react";
+import { SummaryCard, SummaryContainer } from "./styles";
+
+export function Summary() {
+    return (
+        <SummaryContainer>
+            <SummaryCard>
+                <header>
+                    <span>Entradas</span>
+                    <ArrowCircleUp size={32} color="#00b37e" />
+                </header>
+                <strong>R$ 13.130,00</strong>
+            </SummaryCard>
+            <SummaryCard>
+                <header>
+                    <span>Saídas</span>
+                    <ArrowCircleDown size={32} color="#f75a68" />
+                </header>
+                <strong>R$ 13.130,00</strong>
+            </SummaryCard>
+            <SummaryCard>
+                <header>
+                    <span>Total</span>
+                    <ArrowCircleUp size={32} color="#fff" />
+                </header>
+                <strong>R$ 13.130,00</strong>
+            </SummaryCard>
+        </SummaryContainer>
+    )
+}
