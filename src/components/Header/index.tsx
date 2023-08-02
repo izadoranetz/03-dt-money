38804130b196ca310142692cc45727ff3859282d
izadoranetz <izadora.ns@gmail.com>
2023-08-02 17:40:59 -0300

    criando estrutura modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,26 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './style';
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
 
 export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="Logo DT Money" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+                <Dialog.Title>Nova Transações</Dialog.Title>
 
-    return (
-        <HeaderContainer>
-            <HeaderContent>
-                <img src={logoImg} alt="Logo DT Money" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
-            </HeaderContent>
-        </HeaderContainer>
-    );
-
+                <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
